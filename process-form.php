0b329d85aac61bacbc4698ae835bf7e54dc106ae
diego.arevalo <diego.arevalo@globant.com>
2022-10-27 17:59:04 -0500

    Added backend connection

---
+++ process-form.php
@@ -0,0 +1,36 @@
+<?php
+
+$nombre = $_POST["nombre"];
+$cursillo = $_POST["cursillo"];
+$comunidad = $_POST["comunidad"];
+$telefono = $_POST["telefono"];
+$correo = $_POST["correo"];
+$ultreyas = $_POST["ultreyas"];
+$reunionDeGrupo = $_POST["reunionDeGrupo"];
+
+var_dump($nombre, $cursillo, $comunidad, $telefono, $correo, $ultreyas, $reunionDeGrupo);
+
+$host = "localhost";
+$dbname = "registro_db";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect(hostname: $host, 
+                        username: $username, 
+                        password: $password, 
+                        database: $dbname);
+
+if (mysqli_connect_errno()) {
+    die("Connection error: " . mysqli_connect_error());
+}
+
+echo "Connection is succesful.";
+
+$sql = "INSERT INTO registro (nombre, cursillo, comunidad, telefono, correo, ultreyas, reunionDeGrupo)
+        VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+$stmt = mysqli_stmt_init($conn);
+
+if (! mysqli_stmt_prepare($stmt, $sql)) {
+    die(mysqli_error($conn));
+}
